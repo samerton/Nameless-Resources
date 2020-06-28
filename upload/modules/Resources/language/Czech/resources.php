bd4e68fdae2492234094d8fc585f95a8fed4b262
samerton <samerton@users.noreply.github.com>
2020-06-28 10:39:29 +0100

    Add Czech translation
    Thanks to Fjuro

---
+++ upload/modules/Resources/language/Czech/resources.php
@@ -0,0 +1,162 @@
+<?php 
+/*
+ *	Translated by Fjuro
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr3
+ *
+ *  License: MIT
+ *
+ *  Czech Language for Resources module
+ */
+
+$language = array(
+	/*
+	 *  Resources
+	 */ 
+	'resources' => 'Zdroje',
+	'categories' => 'Kategorie',
+	'no_resources' => 'Zatím nebyly přidány žádné zdroje.',
+	'new_resource' => 'Nový zdroj',
+	'select_category' => 'Vyberte kategorii',
+	'github_username' => 'Uživatelské jméno na GitHubu',
+	'github_repo_name' => 'Název GitHub repozitáře',
+	'link_to_github_repo' => 'Odkaz na GitHub repozitář',
+	'required' => 'Vyžadováno',
+	'resource_name' => 'Název',
+	'resource_description' => 'Popis',
+	'version_tag' => 'Verze',
+	'version_tag_help' => 'Musí se shodovat s vaším GitHub release tagem',
+	'contributors' => 'Přispěvatelé',
+	'name_required' => 'Zadejte název zdroje',
+	'content_required' => 'Zadejte popis zdroje',
+	'github_username_required' => 'Zadejte vaše uživatelské jméno na GitHubu',
+	'github_repo_required' => 'Zadejte název vašeho GitHub repozitáře',
+	'version_tag_required' => 'Zadejte verzi vašeho zdroje',
+	'category_required' => 'Vyberte kategorii vašeho zdroje',
+	'name_min_2' => 'Název zdroje musí obsahovat minimálně 2 znaky',
+	'content_min_2' => 'Popis zdroje musí obsahovat minimálně 2 znaky',
+	'github_username_min_2' => 'Vaše GitHub uživatelské jméno musí obsahovat alespoň 2 znaky',
+	'github_repo_min_2' => 'Název vašeho GitHub repozitáře musí obsahovat minimálně 2 znaky',
+	'name_max_64' => 'Název zdroje může obsahovat maximálně 64 znaků',
+	'content_max_20000' => 'Popis zdroje může obsahovat maximálně 20000 znaků',
+	'github_username_max_32' => 'Vaše GitHub uživatelské jméno může obsahovat maximálně 32 znaků',
+	'github_repo_max_64' => 'Název vašeho GitHub repozitáře může obsahovat maximálně 64 znaků',
+	'version_max_16' => 'Verze může obsahovat maximálně 16 znaků',
+	'contributors_max_255' => 'Seznam přispěvatelů může obsahovat maximálně 255 znaků',
+	'unable_to_get_repo' => 'Nepodařilo se získat informaci o posledním vydání z repozitáře {x}. Vytvořili jste na GitHubu vydání?',
+	'update_already_exists' => 'Aktualizace s touto verzí již existuje!',
+	'select_release' => 'Vyberte vydání:',
+	'resource' => 'Zdroj',
+	'stats' => 'Statistiky',
+	'author' => 'Autor',
+	'x_views' => '{x} zobrazení', // Don't replace {x}
+	'x_downloads' => '{x} stažení', // Don't replace {x}
+	'in_category_x' => 'v kategorii {x}', // Don't replace {x}
+	'viewing_resource_x' => 'Prohlížení zdroje {x}', // Don't replace {x}
+	'resource_index' => 'Index zdroje',
+	'reviews' => 'Recenze',
+	'view_other_resources' => 'Zobrazit ostatní zdroje uživatele {x}', // Don't replace {x}
+	'download' => 'Stáhnout',
+	'other_releases' => 'Ostatní vydání',
+	'no_reviews' => 'Žádné recenze',
+	'new_review' => 'Nová recenze',
+	'update' => 'Aktualizovat zdroj',
+	'updated_x' => 'aktualizováno {x}', // Don't replace {x}
+	'viewing_all_releases' => 'Prohlížení všech vydání zdroje {x}', // Don't replace {x}
+	'viewing_release' => 'Prohlížení vydání {x} zdroje {y}', // Don't replace {x} or {y}
+    'editing_resource' => 'Upravování zdroje',
+    'contributors_x' => 'Přispěvatelé: {x}', // Don't replace {x}
+    'move_resource' => 'Přesunout zdroj',
+    'delete_resource' => 'Odstranit zdroj',
+    'confirm_delete_resource' => 'Opravdu chcete odstranit zdroj {x}?', // Don't replace {x}
+    'invalid_category' => 'Vybrali jste neplatnou kategorii',
+    'move_to' => 'Přesunout zdroj do:',
+    'no_categories_available' => 'Neexistují žádné kategorie, kam by se dal tento zdroj přesunout!',
+    'delete_review' => 'Odstranit recenzi',
+    'confirm_delete_review' => 'Opravdu chcete odstranit tuto recenzi?',
+    'viewing_resources_by_x' => 'Prohlížení zdrojů od uživatele {x}', // Don't replace {x}
+	'release_type' => 'Typ vydání',
+    'zip_file' => 'Soubor ZIP',
+    'github_release' => 'GitHub vydání',
+    'type' => 'Typ',
+    'free_resource' => 'Zdarma',
+    'premium_resource' => 'Placené',
+    'price' => 'Cena',
+    'invalid_price' => 'Neplatná cena.',
+    'paypal_email_address' => 'E-mailová adresa PayPal účtu',
+    'paypal_email_address_info' => 'Toto je e-mailová adresa PayPal účtu, na který budou odeslány peníze, když si někdo zakoupí váš placený zdroj.',
+    'invalid_email_address' => 'Zadejte platnou e-mailovou adresu PayPal účtu, mezi 4 a 64 znaky.',
+    'no_payment_email' => 'S vaším účtem není propojena žádná e-mailová adresa PayPal účtu. Můžete ji přidat později v možnostech.',
+    'my_resources' => 'Mé zdroje',
+    'purchased_resources' => 'Zakoupené zdroje',
+    'no_purchased_resources' => 'Zatím jste si nezakoupili žádné zdroje.',
+    'choose_file' => 'Vybrat soubor',
+    'zip_only' => 'Pouze soubory typu .zip',
+    'file_not_zip' => 'Soubor není typu .zip!',
+    'filesize_max_x' => 'Velikost souboru může mít maximálně {x}kb', // Don't replace {x}, unit kilobytes
+	'file_upload_failed' => 'Nepodařilo se nahrát soubor. Chyba: {x}', // Don't replace {x}
+	'purchase_for_x' => 'Zakoupit za {x}', // Don't replace {x}
+	'purchase' => 'Zakoupit',
+    'purchasing_resource_x' => 'Nákup zdroje {x}', // Don't replace {x}
+	'payment_pending' => 'Čekající platba',
+    'update_information' => 'Aktualizovat informace',
+	'paypal_not_configured' => 'Integrace PayPalu ještě nebyla nastavena! Kontaktujte administrátora.',
+	'error_while_purchasing' => 'Omlouváme se! Při zakupování tohoto zdroje se vyskytla chyba. Kontaktujte administrátora.',
+	'author_doesnt_have_paypal' => 'Omlouváme se! Autor zdroje ještě nepropojil svůj účet se svým PayPal účtem.',
+	'sorry_please_try_again' => 'Omlouváme se! Vyskytl se problém, zkuste to znovu později.',
+    'purchase_cancelled' => 'Nákup byl úspěšně zrušen.',
+    'purchase_complete' => 'Platba byla úspěšná. Zdroj bude dostupný po dokončení zpracování platby.',
+    'log_in_to_download' => 'Přihlašte se pro stažení',
+	'external_download' => 'Externí stažení',
+    'external_link' => 'Externí odkaz',
+
+    // Admin
+    'permissions' => 'Oprávnění',
+    'new_category' => '<i class="fa fa-plus-circle"></i> Nová kategorie',
+    'creating_category' => 'Vytváření kategorie',
+    'category_name' => 'Název kategorie',
+    'category_description' => 'Popis kategorie',
+    'input_category_title' => 'Zadejte název kategorie.',
+    'category_name_minimum' => 'Název kategorie musí obsahovat minimálně 2 znaky.',
+    'category_name_maxmimum' => 'Název kategorie může obsahovat maximálně 150 znaků.',
+    'category_description_maximum' => 'Popis kategorie může obsahovat maximálně 250 znaků.',
+    'category_created_successfully' => 'Kategorie byla úspěšně vytvořena.',
+    'category_updated_successfully' => 'Kategorie byla úspěšně aktualizována.',
+    'category_deleted_successfully' => 'Kategorie byla úspěšně odstraněna.',
+    'category_permissions' => 'Oprávnění kategorie',
+    'group' => 'Skupina',
+    'can_view_category' => 'Může zobrazit kategorii?',
+    'can_post_resource' => 'Může vytvářet zdroje?',
+    'moderation' => 'Moderace',
+    'can_move_resources' => 'Může přesouvat zdroje?',
+    'can_edit_resources' => 'Může upravovat zdroje?',
+    'can_delete_resources' => 'Může mazat zdroje?',
+    'can_edit_reviews' => 'Může upravovat recenze?',
+    'can_delete_reviews' => 'Může mazat recenze?',
+    'can_download_resources' => 'Může stahovat zdroje?',
+    'can_post_premium_resource' => 'Může vytvářet placené zdroje?',
+    'delete_category' => 'Odstranit kategorii',
+    'move_resources_to' => 'Přesunout zdroje do',
+    'delete_resources' => 'Odstranit zdroje',
+    'downloads' => 'Stažení',
+    'no_categories' => 'Zatím nebyly vytvořeny žádné kategorie.',
+    'editing_category' => 'Upravování kategorie',
+    'settings' => 'Nastavení',
+    'settings_updated_successfully' => 'Nastavení byla úspěšně aktualizována.',
+    'currency' => 'ISO-4217 měna',
+    'invalid_currency' => 'Neplatná ISO-4217 měna! Seznam platných kódů lze nalézt <a href="https://en.wikipedia.org/wiki/ISO_4217#Active_codes" target="_blank" rel="noopener nofollow">zde</a>',
+    'upload_directory_not_writable' => 'Adresář uploads/resources není zapisovatelný!',
+    'maximum_filesize' => 'Maximální velikost souborů (v kilobytech)',
+    'invalid_filesize' => 'Neplatná velikost souboru!',
+    'pre_purchase_information' => 'Informace před platbou',
+    'invalid_pre_purchase_info' => 'Neplatná předplatební informace! Ujistěte se, že má méně než 100 000 znaků.',
+    'paypal_api_details' => 'Podrobnosti PayPal API',
+    'paypal_api_details_info' => 'Hodnoty těchto polí jsou skryty z bezpečnostních důvodů.<br />Při aktualizaci těchto nastavení zadejte obě hodnoty.',
+    'paypal_client_id' => 'PayPal Client ID',
+    'paypal_client_secret' => 'PayPal Client Secret',
+    'paypal_config_not_writable' => 'Soubor modules/Resources/paypal.php není zapisovatelný. Nelze uložit nastavení PayPalu.',
+
+    // Hook
+    'new_resource_text' => 'Byl vytvořen nový zdroj v {x} uživatelem {y}',
+    'updated_resource_text' => 'Byl aktualizován zdroj v {x} uživatelem {y}'
+);
