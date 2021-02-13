633226716fc460b52c030474af08b57da6a4b8d9
Sam <samerton@users.noreply.github.com>
2021-02-13 22:25:21 +0000

    1.3.0

--- upload/modules/Resources/module.php
+++ upload/modules/Resources/module.php
@@ -18,7 +18,7 @@ class Resources_Module extends Module {
 
 		$name = 'Resources';
 		$author = '<a href="https://samerton.me" target="_blank">Samerton</a>';
-		$module_version = '1.2.2';
+		$module_version = '1.3.0';
 		$nameless_version = '2.0.0-pr9';
 
 		parent::__construct($this, $name, $author, $module_version, $nameless_version);
