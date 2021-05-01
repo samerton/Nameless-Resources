4f77b9df9cd6d676abe90d34603d68d8d4e0223d
Boombastic20 <73121093+Boombastic20@users.noreply.github.com>
2021-05-01 02:03:18 +0000

    add tag for resource
    add tag for resource

--- upload/modules/Resources/pages/resources/index.php
+++ upload/modules/Resources/pages/resources/index.php
@@ -108,6 +108,7 @@ if(count($latest_releases)){
 			$releases_array[$resource->id] = array(
 				'link' => URL::build('/resources/resource/' . $resource->id . '-' . Util::stringToURL($resource->name)),
 				'name' => Output::getClean($resource->name),
+				'tag' => Output::getClean($resource->tag),
 				'description' => mb_substr(strip_tags(Output::getDecoded($resource->description)), 0, 60) . '...',
 				'author' => Output::getClean($resource_author->getDisplayname()),
 				'author_style' => $resource_author->getGroupClass(),
